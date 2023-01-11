1c55a05e080b4d238c974d25ec16c376a92ff4c2
diabeticMike <tmymichael@gmail.com>
2023-01-11 17:50:08 +0200

    provide some detailed seeds for further examples

--- sql/relations/one-to-many/init.sql
+++ sql/relations/one-to-many/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE fathers (
 CREATE TABLE children (
     id uuid primary key,
     name text NOT NULL UNIQUE,
-    father_id uuid NOT NULL references fathers (id) on delete cascade
+    father_id uuid references fathers (id) on delete cascade
 );
 
 INSERT INTO fathers (
@@ -25,6 +25,10 @@ INSERT INTO fathers (
 (
     'ed75f0e9-ca28-4741-af93-a459ddbabe08',
     'German'
+),
+(
+    'ed85f0e9-ca28-4741-af93-a459ddbabe08',
+    'Dave'
 );
 
 INSERT INTO children (
@@ -45,4 +49,9 @@ INSERT INTO children (
     'b61756fe-0d4b-452e-9189-0d49664116d8',
     'Stacy',
     '95285f8f-4880-4258-8712-a622f413bd30'
+),
+(
+    'a81756fe-0d4b-452e-9189-0d49664116d8',
+    'John',
+    NULL
 );
