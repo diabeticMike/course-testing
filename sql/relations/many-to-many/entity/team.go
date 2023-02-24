9fa2da4a28c44e53ef6aab1bb802418fae5c21b1
Mykhailo Tkachuk <diabeticmike@MacBook-Pro-Mykhailo.local>
2023-02-24 18:53:22 +0200

    change gorm tags to suit new schema names

--- sql/relations/many-to-many/entity/team.go
+++ sql/relations/many-to-many/entity/team.go
@@ -5,5 +5,9 @@ import "github.com/google/uuid"
 type Team struct {
 	ID    uuid.UUID `json:"id"`
 	Name  string    `json:"name"`
-	Users []User    `gorm:"many2many:users_teams;" json:"users,omitempty"`
+	Users []User    `gorm:"many2many:user_team;" json:"users,omitempty"`
+}
+
+func (Team) TableName() string {
+	return "team"
 }
--- sql/relations/many-to-many/entity/user.go
+++ sql/relations/many-to-many/entity/user.go
@@ -5,5 +5,9 @@ import "github.com/google/uuid"
 type User struct {
 	ID    uuid.UUID `json:"id"`
 	Name  string    `json:"name"`
-	Teams []Team    `gorm:"many2many:users_teams;" json:"teams,omitempty"`
+	Teams []Team    `gorm:"many2many:user_team;" json:"team,omitempty"`
+}
+
+func (User) TableName() string {
+	return "user"
 }
